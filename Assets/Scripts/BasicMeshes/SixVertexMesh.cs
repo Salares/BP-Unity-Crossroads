a6022526965fbd978ccfe6b89215ecdcf29bee74
Daniel Koláček <daniel.kolacek@issa.cz>
2023-03-28 16:44:04 +0200

    Procedural mesh examples
    Triangles, Quads, Cubes

---
+++ Assets/Scripts/BasicMeshes/SixVertexMesh.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class SixVertexMesh : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+
+    void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+
+    }
+
+    void Update()
+    {
+        CreateMeshData();
+        CreateMesh();
+    }
+
+    private void CreateMeshData()
+    {
+        vertices = new Vector3[] {  new Vector3(0,YValue.ins.yValue,0),new Vector3(0,0,1),new Vector3(1,0,0), 
+                                    new Vector3(1,0,0),new Vector3(0,0,1),new Vector3(1,0,1)};
+        triangles = new int[] {0,1,2,3,4,5};
+    }
+
+        private void CreateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+}
