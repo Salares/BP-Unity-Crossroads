7caa33bbfd2e83f5d0127189b8cd64c2c5d2f64e
Daniel Koláček <daniel.kolacek@issa.cz>
2023-04-18 14:07:29 +0200

    Crossroad mesh fill - no curves

--- Assets/Scripts/Crossroads/Examples/CrossroadPlacer.cs
+++ Assets/Scripts/Crossroads/Examples/CrossroadPlacer.cs
@@ -60,8 +60,15 @@ public class CrossroadPlacer : MonoBehaviour
 
     private Mesh CreateCrossroadMesh()
     {
-        Vector3[] vertices = ConvertToVector3(crossroadVertices.ToArray());
-        int[] triangles = new int[(vertices.Length - 2) * 3];
+        Vector3[] verticesNoCenter = ConvertToVector3(crossroadVertices.ToArray());
+        Vector3[] vertices = new Vector3[1 + verticesNoCenter.Length];
+        vertices[0] = this.transform.position;
+        verticesNoCenter.CopyTo(vertices, 1);
+
+        //vert 9
+        //tri 8 * 3
+
+        int[] triangles = new int[(vertices.Length - 1) * 3];
 
         Mesh mesh = new Mesh();
 
@@ -69,11 +76,32 @@ public class CrossroadPlacer : MonoBehaviour
         int triangleIndex = 0;
         for (int i = 1; i < vertices.Length - 1; i++)
         {
-            triangles[triangleIndex] = 0;
-            triangles[triangleIndex + 1] = i;
+            triangles[triangleIndex] = i;
+            triangles[triangleIndex + 1] = 0;
             triangles[triangleIndex + 2] = i + 1;
             triangleIndex += 3;
+            
         }
+
+        triangles[triangleIndex] = vertices.Length - 1;
+        triangles[triangleIndex + 1] = 0;
+        triangles[triangleIndex + 2] = 1;
+
+        Debug.Log(triangles.Length);
+        Debug.Log(vertices.Length);
+
+        // triangles[triangleIndex] = vertices.Length;
+        // triangles[triangleIndex + 1] = 0;
+        // triangles[triangleIndex + 2] = 1;
+
+        // Debug.DrawLine(vertices[0],vertices[1], Color.cyan, 10f);
+        // Debug.DrawLine(vertices[1],vertices[2], Color.magenta, 10f);
+        // Debug.DrawLine(vertices[2],vertices[3], Color.yellow, 10f);
+        // Debug.DrawLine(vertices[3],vertices[4], Color.black, 10f);
+        // Debug.DrawLine(vertices[4],vertices[5], Color.red, 10f);
+        // Debug.DrawLine(vertices[5],vertices[6], Color.green, 10f);
+        // Debug.DrawLine(vertices[6],vertices[7], Color.blue, 10f);
+        // Debug.DrawLine(vertices[7],vertices[0], Color.white, 10f);
 
         // Set the vertices and triangles to the mesh
         mesh.vertices = vertices;
@@ -144,9 +172,9 @@ public class CrossroadPlacer : MonoBehaviour
             triangleIndex += 6;
         }
         Mesh mesh = new Mesh();
-        Debug.DrawLine(vertices[0],vertices[1], Color.magenta, 10f);
-        crossroadVertices.Add(vertices[0]);
+        
         crossroadVertices.Add(vertices[1]);
+        crossroadVertices.Add(vertices[0]);
         mesh.vertices = vertices;
         mesh.uv = uvs;
         mesh.triangles = triangles;
